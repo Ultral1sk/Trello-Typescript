25caa3743e1751bf75e368acdfaf988299c50a35
Ultral1sk <jderebanoski@gmail.com>
2020-06-16 23:04:51 +0200

    webpack configuration with reac with TpyeScript working

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/app.css'
+
+const App: React.FC = () => {
+      return (
+      <div>
+            <h1>Hello World!</h1>
+            
+      </div>
+      )
+}
+
+export default App
